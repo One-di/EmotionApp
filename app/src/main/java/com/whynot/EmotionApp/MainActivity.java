2dd981b4d0077ae09f2498875dbb5ecf5dec6174
One-di <One-di@users.noreply.github.com>
2021-03-01 00:11:31 +0300

    add select image

--- app/src/main/java/com/whynot/EmotionApp/MainActivity.java
+++ app/src/main/java/com/whynot/EmotionApp/MainActivity.java
@@ -1,14 +1,71 @@
 package com.whynot.EmotionApp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    static final int GALLERY_REQUEST = 1;
+    static final Bitmap DEAFAULT_IMAGE = null;
+    Bitmap bitmap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button buttonSelectImage = findViewById(R.id.button1);
+        Button buttonSendImage = findViewById(R.id.button2);
+
+        buttonSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
+            }
+        });
+
+        buttonSendImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bitmap == null){
+                    System.out.println("");
+                }else{
+
+                }
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        ImageView imageView = findViewById(R.id.imageView1);
+
+        if (requestCode == GALLERY_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Uri selectedImage = imageReturnedIntent.getData();
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+
 }
